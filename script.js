fe00b06365297b6bc88649174e8535891aaeae92
larissa <larissaleonso51@gmail.com>
2019-07-25 16:26:56 -0300

    animaçao do jogo das cores

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-let numero = Math.round(2.4); //arredonda para o mais proxim
+let quadradinhos = document.querySelectorAll("div");
+let resposta = document.querySelector("h2");
+let botao = document.querySelector("button");
 
-numero = Math.floor(1625.9); //arredonda para baixo
+//CRIANDO LISTA DE CORES
+let cores = ["red", "blue", "yellow"];
 
-numero = Math.ceil (25.5); //arredonda para cima
 
-numero = Math.pow(5,5); //expoente
+//FUNÇÃO PARA GERAR CORES ALEATORIAS
+function gerarCor(){
+    let aleatorio = Math.floor(Math.random()* cores.length);
+    //o THIS está sendo utilizando para se referenciar ao próprio elemento em que está sendo clicado
+    this.style.backgroundColor = cores[aleatorio];
 
-numero = Math.sqrt(144); //raiz
+    //chamando a função de verificar final, para verificar somente quando gerar uma cor
+    verificarFinal();
+}
+//Aplicando a função à todos os quadradinhos
+for(let quadrado of quadradinhos){
+    quadrado.onclick = gerarCor;
+}
 
-numero = Math.PI;
+//Função que verifica se todos os quadrados estão com a mesma cor
+function verificarFinal(){
+    if(quadradinhos[0].style.backgroundColor == quadradinhos[1].style.backgroundColor && quadradinhos[1].style.backgroundColor == quadradinhos[2].style.backgroundColor && quadradinhos[2].style.backgroundColor == quadradinhos[3].style.backgroundColor){
+        resposta.innerHTML = "!!! VOCÊ VENCEU !!!";
 
-numero = Math.sin(0*Math.PI / 4); //sin,ceno ou cos
+        for(let quadrado of quadradinhos){
+            quadrado.onclick = null;
+            quadrado.classList.add("virarQuadradinho");
+        }  
+    }
+    else{
+        resposta.innerHTML= "Ainda não foi !!";
+    }
+}
 
-console.log(numero);
+//Função para resetar o jogo, limpando as cores de fundo e trocando o texto de resposta
+function resetarJogo(){
+    for(let quadrado of quadradinhos){
+        quadrado.style.backgroundColor = "unset";
+    }
+    resposta.innerHTML = "Jogo resetado !";
+}
+
+botao.ondblclick = resetarJogo;
+
 
 
 
