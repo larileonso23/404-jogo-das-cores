3559427316646ae2f62a4ece7c88e3a8be9dc04b
larissa <larissaleonso51@gmail.com>
2019-07-25 15:48:31 -0300

    codigos sobre os numeros script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let numero = Math.round(2.4); //arredonda para o mais proxim
+
+numero = Math.floor(1625.9); //arredonda para baixo
+
+numero = Math.ceil (25.5); //arredonda para cima
+
+numero = Math.pow(5,5); //expoente
+
+numero = Math.sqrt(144); //raiz
+
+numero = Math.PI;
+
+numero = Math.sin(0*Math.PI / 4); //sin,ceno ou cos
+
+console.log(numero);
+
+
+
+
